09d5bd7775b98225743d42b82a3b8a0185253f78
Tony Josi <tonyjosinew@gmail.com>
2017-06-21 20:36:44 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -6,11 +6,6 @@ using namespace std;
 void merge(vector<int> & num, vector<int> & weights, int l, int r, int m)
    {
 
-
-
-
-
-
       int li,ri,sizel,sizer;
       sizel = m - l + 1;
       sizer = r - m;
@@ -18,15 +13,8 @@ void merge(vector<int> & num, vector<int> & weights, int l, int r, int m)
       int rar[sizer];
 
 
-
-
-      //new
       int lnum[sizel];
       int rnum[sizer];
-
-
-
-
 
 
       for(int i = 0; i < sizel; i++)
@@ -42,21 +30,41 @@ void merge(vector<int> & num, vector<int> & weights, int l, int r, int m)
       li = 0;
       ri = 0;
       int k = l;
+
       while((li < sizel) && (ri < sizer))
       {
-         if(lar[li] >= rar[ri])
+
+         if(lar[li] > rar[ri])
          {
             weights[k] = lar[li];
             num[k] = lnum[li];
             li++;
          }
+
+         else if(lar[li] == rar[ri])
+         {
+            weights[k] = lar[li];
+            if(lnum[li] > rnum[ri])
+            {
+               num[k] = lnum[li];
+               li++;
+            }
+            else
+            {
+               num[k] = rnum[ri];
+               ri++;
+            }
+         }
+
          else
          {
             weights[k] = rar[ri];
             num[k] = rnum[ri];
             ri++;
          }
+
          k++;
+
       }
       while(li < sizel)
       {
@@ -120,6 +128,11 @@ int main()
    for(i = 0; i < size; i++)
    {
       cout<<num[i]<<"\n";
+   }
+   cout<<"\n\nWeights:\n";
+   for(i = 0; i < size; i++)
+   {
+      cout<<weights[i]<<"\n";
    }
    return 0;
 }
